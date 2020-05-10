58c833bd2c73bbb14b459d36a70bc39e3b8ef1ec
Thibaud Kehler <mail@tkehler.de>
2020-05-10 19:17:29 +0200

    Implement fireforget, modularized configs

--- src/settings.rs
+++ src/settings.rs
@@ -1,12 +1,22 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use config::{ConfigError, Config, File};
 use std::collections::HashMap;
 use serde::Deserialize;
+use std::{process, env};
+use dirs;
+
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
     pub fps: u8,
+    pub bindings: HashMap<String, BindingConfig>,
     pub tracks: HashMap<String, TrackConfig>,
+
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BindingConfig {
+    pub binding: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -16,13 +26,35 @@ pub struct TrackConfig {
 }
 
 impl Settings {
-    pub fn new(config: &Path) -> Result <Self, ConfigError> {
-        let mut s = Config::new();
+    pub fn new() -> Result<Self, ConfigError> {
+        let mut settings = Config::new();
 
-        s.merge(File::from(config));
+        // Default settings
+        settings.set("fps", "60");
 
+        // Load user config
+        if let Some(mut config_dir) = dirs::config_dir() {
+            config_dir.push("sound_board");
+            config_dir.push("config.toml");
+            if config_dir.exists() {
+                settings.merge(File::from(config_dir.clone())).expect(format!("Failed to read config file {}!", config_dir.display()).as_str());
+            } else { eprintln!("No user config file found!"); }
+        } else { eprintln!("No config directory found!"); }
+
+
+        // Load project config
+        let mut args: Vec<String> = env::args().collect();
+        assert_eq!(args.len(), 2, "sound_board expects exactly one argument!\nUsage: soundboard path/to/project/config.toml");
+        let arg = &args.pop().expect("Failed to get config path from args");
+        let project_config_path = Path::new(arg);
+
+        if project_config_path.exists() {
+            settings.merge(File::from(project_config_path)).expect(format!("Failed to read config file {}!", project_config_path.display()).as_str());
+        } else {
+            eprintln!("Config file {} not found!", project_config_path.display());
+            process::exit(1);
+        }
         //println!("settings: {:?}", s);
-
-        s.try_into()
+        settings.try_into()
     }
 }
--- src/tracks/mod.rs
+++ src/tracks/mod.rs
@@ -1,36 +1,47 @@
 mod toggle;
+mod fire_and_forget;
 
 pub use toggle::ToggleTrack;
 use crate::launchpad::{ButtonEvent, Button};
 use crate::sound_board::EventHandler;
 use crate::settings::TrackConfig;
 use crate::launchpad::colors::RGColor;
+use crate::tracks::fire_and_forget::FireForgetTrack;
 
 pub type Track<C> = dyn EventHandler<ButtonEvent, C>;
 
 
-pub fn from_config(button_name: String, config: TrackConfig, audio_device: &rodio::Device) -> (Box<Track<RGColor>>, RGColor) {
+pub fn from_config<'a>(button_name: String, config: TrackConfig, audio_device: &'a rodio::Device) -> (Box<dyn EventHandler<ButtonEvent, RGColor> + 'a> , RGColor) {
     let mode = Mode::from(config.mode.as_str());
 
-    let (track, initial_color) = match mode {
-        Mode::Toggle => ToggleTrack::new(audio_device, button_name, config.path),
+    return match mode {
+        Mode::Toggle => {
+            let (track, initial_color) = ToggleTrack::new(audio_device, button_name, config.path);
+             (Box::new(track), initial_color)
+        },
+        Mode::FireForget => {
+            let (track, initial_color) = FireForgetTrack::new(audio_device, button_name, config.path);
+            (Box::new(track), initial_color)
+        }
     };
-    return (Box::new(track), initial_color);
+
 }
 
 
 pub enum Mode {
     Toggle,
-    // FireForget,
+    FireForget,
     // Fire,
     // Hold,
+    // Loop,
+
 }
 
 impl From<&str> for Mode {
     fn from(str: &str) -> Self {
         match str {
             "toggle" => Mode::Toggle,
-            //  "fireforget" => Mode::FireForget,
+            "fireforget" => Mode::FireForget,
             //  "fire" => Mode::Fire,
             //  "hold" => Mode::Hold,
             _ => panic!("Illegal mode '{}'", str)
